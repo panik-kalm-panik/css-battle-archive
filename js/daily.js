36c0f0881f4516b97ff6f1985036f0a56b067fcb
Panik <30728714+panik-kalm-panik@users.noreply.github.com>
2023-11-06 23:12:56 +0100

    Minor changes in website
    Website:
    - Smaller iframe and code elements
    File tree:
    - Ordered by year/month/day
    Daily Nov 6th added.

--- js/daily.js
+++ js/daily.js
@@ -2,11 +2,16 @@ var daily_array = [
     {
         'date': 'November 4th',
         'year': '2023',
-        'file': './archive/daily_challenges/nov-04.html'
+        'file': './archive/daily_challenges/2023/11/04.html'
     },
     {
         'date': 'November 5th',
         'year': '2023',
-        'file': './archive/daily_challenges/nov-05.html'
+        'file': './archive/daily_challenges/2023/11/05.html'
+    },
+    {
+        'date': 'November 6th',
+        'year': '2023',
+        'file': './archive/daily_challenges/2023/11/06.html'
     },
 ];
